149ca6704a941bf74b56e9c5d200b55470070c8d
Roman <roman.lopez@outlook.com>
2017-06-17 13:44:55 -0500

    validacion de ceros y vacio en especiales y normal

--- asignar.php
+++ asignar.php
@@ -26,33 +26,40 @@ if(
     $monto=filter_var($_POST['monto'],FILTER_SANITIZE_NUMBER_FLOAT,FILTER_FLAG_ALLOW_FRACTION);
     $nombreAsesor=filter_var($_POST['nombreAsesor'],FILTER_SANITIZE_STRING);
 
-    //db
-    $db=new Conexion();
+    if($cantidad>0 && $monto>0){
+        //db
+        $db=new Conexion();
 
-    $sentencia="{CALL spAsignarComision(?,?,?,?,?,?)}";
-    $parametros=array(
-        array($agencia,SQLSRV_PARAM_IN),
-        array($producto,SQLSRV_PARAM_IN),
-        array($cantidad,SQLSRV_PARAM_IN),
-        array($asesor,SQLSRV_PARAM_IN),
-        array($nombreAsesor,SQLSRV_PARAM_IN),
-        array($monto,SQLSRV_PARAM_IN)
-    );
-	
-    $resp=$db->query($sentencia,$parametros);
+        $sentencia="{CALL spAsignarComision(?,?,?,?,?,?)}";
+        $parametros=array(
+            array($agencia,SQLSRV_PARAM_IN),
+            array($producto,SQLSRV_PARAM_IN),
+            array($cantidad,SQLSRV_PARAM_IN),
+            array($asesor,SQLSRV_PARAM_IN),
+            array($nombreAsesor,SQLSRV_PARAM_IN),
+            array($monto,SQLSRV_PARAM_IN)
+        );
 
-    if($resp){
-        $datos=$db->rows($resp);
-        $respuesta[]=array('datos'=>$datos[0],'status'=>true);
-        //echo json_encode(array('datos'=>$datos,'status'=>true));
+        $resp=$db->query($sentencia,$parametros);
+
+        if($resp){
+            $datos=$db->rows($resp);
+            $respuesta[]=array('datos'=>$datos[0],'status'=>true);
+            //echo json_encode(array('datos'=>$datos,'status'=>true));
+        }
+        //hubo errores?
+        $errores=$db->errors();
+        if(!is_null($errores)){
+            echo utf8_encode($errores[0]['message']);
+        }
+        //var_dump(sqlsrv_errors());
+        $db->cerrar();
     }
-	//hubo errores?
-	$errores=$db->errors();
-	if(!is_null($errores)){
-		echo utf8_encode($errores[0]['message']);
-	}
-	//var_dump(sqlsrv_errors());
-    $db->cerrar();
+    else{
+        $respuesta[]=array('status'=>false,'mensaje'=>'No se permiten valores en cero o vacios en comision normal.');
+    }
+
+
 
     //existen comisones especiales?
     if(!empty($_POST['asesor'])){
@@ -60,6 +67,7 @@ if(
         $nombresAsesores=filter_var_array($_POST['asesorNombre'],FILTER_SANITIZE_STRING);
         $cantidadComEsp=filter_var_array($_POST['cantidadExtra'],FILTER_SANITIZE_NUMBER_INT);
         $montoExtra=filter_var_array($_POST['montoExtra'],FILTER_SANITIZE_NUMBER_FLOAT,FILTER_FLAG_ALLOW_FRACTION);
+
         for($i=0;$i<count($asesores);$i++){
             if($asesores[$i]>0){
                 $idAsesorComEspX=$asesores[$i];
@@ -80,23 +88,30 @@ if(
                     array($comEspNumX,SQLSRV_PARAM_IN)
                 );
 
-                $db=new Conexion();
-                $respComEsp=$db->query($sentenciaEsp,$parametrosEsp);
-                if($respComEsp){
-                    print_r(sqlsrv_errors());
-                    $datos=$db->rows($respComEsp);
-                    //echo json_encode(array('datos'=>$datos,'status'=>true));
-                    $respuesta[]=array('datos'=>$datos[0],'status'=>true);
+                //solo asignar si la cantidad y el monto es diferente a cero
+                if($cantidadComEspX>0 && $montoComEspX>0){
+                    $db=new Conexion();
+                    $respComEsp=$db->query($sentenciaEsp,$parametrosEsp);
+                    if($respComEsp){
+                        print_r(sqlsrv_errors());
+                        $datos=$db->rows($respComEsp);
+                        //echo json_encode(array('datos'=>$datos,'status'=>true));
+                        $respuesta[]=array('datos'=>$datos[0],'status'=>true);
+                    }
+                    else{print_r(sqlsrv_errors());}
+                    $db->cerrar();
                 }
-                else{print_r(sqlsrv_errors());}
-                $db->cerrar();
+                else{
+                    $respuesta[]=array('status'=>false,'mensaje'=>'No se permiten valores en cero o vacios en comisiones especiales.');
+                }
+
             }
         }
     }
     echo json_encode($respuesta);
 }
 else{
-    echo json_encode(array("status"=>false,"text"=>"Error de campos requeridos"));
+    echo json_encode(array("status"=>false,"mensaje"=>"Error de campos requeridos"));
 }
 
 
